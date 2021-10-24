fbb1339625121137407ed678287a1bf83d7021b4
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-10-24 10:59:17 +0530

    Update 6.APandGP.cpp.cpp

--- 1.mathematics_for_cp/6.APandGP.cpp.cpp
+++ 1.mathematics_for_cp/6.APandGP.cpp.cpp
@@ -15,5 +15,15 @@ int main()
     cin >> n;
     cout << " nth term of Ap is " << (a + (n - 1) * d) << endl;
     cout << " sum n term of Ap is : " << n * (2 * a + (n - 1) * d) / 2;
+    // return 0;
+    // GP is a special sequence in which elements are arranged
+    //  in such a way that first element is multiplied by
+    //  common ratio
+    int f, r;
+    cout << "enter the first element of gp and common ratio : ";
+    cin >> f >> r;
+    cout << endl;
+    cout << " mth term of gp is : " << (a * pow(r, m)) << endl;
+    cout << " sum of m term is : " << a(pow(r, n) - 1) / (r - 1) << endl;
     return 0;
 }
