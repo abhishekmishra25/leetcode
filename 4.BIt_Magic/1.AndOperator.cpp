46363d2cf805eb43b90318075a24da1d153d87c4
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-10-25 18:19:24 +0530

    updated

--- 4.BIt_Magic/1.AndOperator.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-    int x = 3, y = 6;
-    cout << (x & y) << " ";
-}
---
+++ 4.BIt_Magic/1.BitwiseOperator.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x = 3, y = 6;
+    // bitwise AND operator
+    cout << (x & y) << " ";
+    // bitwise Or operator
+    cout << (x | y) << " ";
+    // bitwise XOR operator
+    cout << (x ^ y) << " ";
+}
