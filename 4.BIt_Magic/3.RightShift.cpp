5312a3abbd9014923d933d8df3243b6d172f97fe
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-10-25 23:52:15 +0530

    added file

---
+++ 4.BIt_Magic/3.RightShift.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x = 33;
+    cout << (x >> 1) << endl;
+    cout << (x >> 2) << endl;
+    int y = 4;
+    cout << (x >> y) << endl;
+    return 0;
+}
