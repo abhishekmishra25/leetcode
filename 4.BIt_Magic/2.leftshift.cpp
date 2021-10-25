8d53fd78d58e2d1b7e2dd95ae7db4170d8200047
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-10-25 23:38:13 +0530

    updated

---
+++ 4.BIt_Magic/2.leftshift.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x = 3;
+    cout << (x << 1) << endl;
+    cout << (x << 2) << endl;
+    int y = 4;
+    cout << (x << y) << endl;
+}
