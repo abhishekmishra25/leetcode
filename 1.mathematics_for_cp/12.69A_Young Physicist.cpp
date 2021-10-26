87c349bf41e8522c12230a1ddd4af98fb6f85183
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-10-26 19:25:02 +0530

    Create 12.69A_Young Physicist.cpp

---
+++ 1.mathematics_for_cp/12.69A_Young Physicist.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int sumx = 0, sumy = 0, sumz = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int x = 0, y = 0, z = 0;
+        cin >> x >> y >> z;
+        sumx += x;
+        sumy += y;
+        sumz += z;
+    }
+    if (sumx == 0 && sumz == 0 && sumy == 0)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
