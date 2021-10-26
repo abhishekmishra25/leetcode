7fd578d9f5b79379c4d6142f6bd162a11743038b
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-10-26 20:06:31 +0530

    Update 5.1.CountSetbit2.cpp

--- 4.BIt_Magic/5.1.CountSetbit2.cpp
+++ 4.BIt_Magic/5.1.CountSetbit2.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++>
+#include <bits/stdc++.h>
 using namespace std;
 int table[256];
 void initialize()
@@ -11,4 +11,15 @@ void initialize()
 }
 int main()
 {
+    int n;
+    cin >> x;
+    int res = table[n & 0xff];
+    n = n >> 8;
+    res += table[n & 0xff];
+    n = n >> 8;
+    res += table[n & 0xff];
+    n = n >> 8;
+    res += table[n & 0xff];
+    cout << res << endl;
+    return 0;
 }
