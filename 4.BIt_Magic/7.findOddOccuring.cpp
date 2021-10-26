b2736185b9beef444a6329a58afbbf9be879b3c8
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-10-26 23:49:21 +0530

    updated

--- 4.BIt_Magic/7.findOddOccuring.cpp
+++ 4.BIt_Magic/7.findOddOccuring.cpp
@@ -1,5 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+void findmissing(int arr[], int n)
+{
+    int x = 0;
+    for (int i = 1; i < n; i++)
+    {
+        x = x ^ i ^ arr[i - 1];
+    }
+    x = x ^ n;
+    cout << x << endl;
+}
 int main()
 {
     int arr[] = {2, 4, 3, 4, 5, 5, 8, 9, 8, 9, 2};
@@ -9,5 +19,7 @@ int main()
         x = x ^ arr[i];
     }
     cout << x << endl;
+    int brr[] = {1, 3, 4};
+    findmissing(brr, 4);
     return 0;
 }
