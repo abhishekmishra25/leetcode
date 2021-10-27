5c1823a780cec17b0b9886cf5fb0c01c8e466237
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-10-27 22:26:58 +0530

    Update 8.finddistinct.cpp

--- 4.BIt_Magic/8.finddistinct.cpp
+++ 4.BIt_Magic/8.finddistinct.cpp
@@ -1,7 +1,18 @@
+
+// this programm is desinged for two distint elemnet in array
+// in order to achive that we need to xor all element
+//  then need to find last set bit
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int arr[] = {1, 2, 3, 4, 1, 2, 3, 3, 4, 4, 5, 6, 5, 6};
-    int n = 11;
+    int n = 14;
+    int x = 0, res1 = 0;
+    res2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        x = x ^ arr[i];
+    }
+    int sbl = x & (~x); // to find the last set bit if result
 }
