6263655cdf454421752043728cb9b3420783c3b7
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-10-27 22:32:44 +0530

    Update 8.finddistinct.cpp

--- 4.BIt_Magic/8.finddistinct.cpp
+++ 4.BIt_Magic/8.finddistinct.cpp
@@ -11,8 +11,7 @@ int main()
 {
     int arr[] = {1, 2, 3, 4, 1, 2, 3, 3, 4, 4, 5, 6, 5, 6};
     int n = 14;
-    int x = 0, res1 = 0;
-    res2 = 0;
+    int x = 0, res1 = 0, res2 = 0;
     for (int i = 0; i < n; i++)
     {
         x = x ^ arr[i];
