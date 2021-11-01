7e43f2ea3f162b26619471684eb5c8f0bf942be9
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-01 17:26:16 +0530

    Update 4.factorial.cpp

--- 5.Recursion/4.factorial.cpp
+++ 5.Recursion/4.factorial.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 #define ll long long
 
+// this programm not only takes lareg time
+// but also take O(n) extra space because it stores all
+// fuction call in the stack
 ll fect(int n)
 {
     if (n == 0)
