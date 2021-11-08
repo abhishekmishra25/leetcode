070993e56339f3aa76504cc4cb12cdd031aaad0a
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-08 22:57:33 +0530

    updated

--- 5.Recursion/6.palindrome.cpp
+++ 5.Recursion/6.palindrome.cpp
@@ -7,21 +7,13 @@ bool recursive(string str, int start, int end)
     {
         return true;
     }
-
-    else if (str[start] == str[end - 1])
-    {
-        recursive(str, start + 1, end - 1);
-    }
-    else
-    {
-        return false;
-    }
+    return (str[start] == str[end]) && recursive(str, start + 1, end - 1);
 }
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     string str = "abbcbba";
-    cout << recursive(str, str.size(), 0);
+    cout << recursive(str, 0, str.size() - 1);
     return 0;
 }
