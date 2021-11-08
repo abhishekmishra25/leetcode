90adc0dd59f5bde1513d7d51a037e335b610e4eb
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-08 23:41:14 +0530

    Update 7.RopeCutting.cpp

--- 5.Recursion/7.RopeCutting.cpp
+++ 5.Recursion/7.RopeCutting.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 int ropeCutting(int n, int a, int b, int c)
 {
-    if (n == 0)
+    if (n < 0)
         return -1;
+
+    return max(ropeCutting(n - 1, a, b, c), ropeCutting(n - b, a, b, c), ropeCutting(n - c, a, b, c));
 }
 
 int main()
