0c696a62808fedaf6ac82e3f5c4749644ef97f75
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-08 23:20:31 +0530

    added

---
+++ 5.Recursion/7.RopeCutting.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int ropeCutting(int n, int a, int b, int c)
+{
+    if (n == 0)
+        return -1;
+}
+
+int main()
+{
+    int n, a, b, c;
+    return 0;
+}
