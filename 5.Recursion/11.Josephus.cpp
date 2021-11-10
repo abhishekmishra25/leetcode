5784306fee934e68df806bae4cb0f69db4c6dafd
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-10 19:36:39 +0530

    Update 11.Josephus.cpp

--- 5.Recursion/11.Josephus.cpp
+++ 5.Recursion/11.Josephus.cpp
@@ -5,11 +5,22 @@ using namespace std;
 // In this problem we are given n number of
 // person , each time we have to kill the kth person
 
-void josephus(int n, int k)
+void josephus(int arr[], int n, int k, int start = 0)
+{
+    if (start == k)
+    {
+    }
+}
 {
 }
 
 int main()
 {
-    int n;
+    int n = 7, k = 3;
+    vector<int> m(n);
+    for (int i = 0; i < n; i++)
+    {
+        v[i] = i;
+    }
+    josephus(m, n, k)
 }
