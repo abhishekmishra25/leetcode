e96e880ae218591bfda78b66655229daff9c8a9f
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-10 05:38:57 +0530

    Update 9.towerrofHanoi.cpp

--- 5.Recursion/9.towerrofHanoi.cpp
+++ 5.Recursion/9.towerrofHanoi.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// tower of hanoi is considred a hard problem because
+// it is mearly hard to draw recursion tree
+// so it very hard and conceptiual
+
 void TowerofHanoi(int a, int b, int c)
 {
     if (a == 0)
