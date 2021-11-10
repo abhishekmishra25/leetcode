169639a1201262ae6124bda9be80f59fe7674c29
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-10 19:22:11 +0530

    Update 11.Josephus.cpp

--- 5.Recursion/11.Josephus.cpp
+++ 5.Recursion/11.Josephus.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+// In this problem we are given n number of
+// person , each time we have to kill the kth person
+
+void josephus(int n, int k)
+{
+}
+
 int main()
 {
     int n;
