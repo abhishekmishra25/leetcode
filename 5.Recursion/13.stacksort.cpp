497e8f7417d0849c4e42ef7bce461772e2c4b250
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-11 21:26:43 +0530

    Update 13.stacksort.cpp

--- 5.Recursion/13.stacksort.cpp
+++ 5.Recursion/13.stacksort.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
 using namesapce std;
 
+void StackSort(stack<int> &s)
+{
+    // using multimap method
+    multimap<int> m;
+    while (!s.empty())
+    {
+    }
+}
+
 int main()
 {
     int n;
+    stack<int> s;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        s.push(x);
+    }
 }
