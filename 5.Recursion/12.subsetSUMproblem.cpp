da619bff5be0c00f56d89f5c8fd686a531297d44
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-11 20:06:29 +0530

    updated

--- 5.Recursion/12.subsetSUMproblem.cpp
+++ 5.Recursion/12.subsetSUMproblem.cpp
@@ -1,26 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int subsetSUM(vector<int> v, int n, int kum, int curr = 0)
+int subsetSUM(int v[], int n, int kum)
 {
     if (n == 0)
     {
-        if curr == kum)
-            {
-                return 1;
-            }
-        else
-        {
-            return 0;
-        }
+        return kum == 0 ? 1 : 0;
     }
-    return subsetSUM(v, n - 1, kum, curr += v[n - 1]) + subsetSUM(v, n - 1, kum, curr);
+    return subsetSUM(v, n - 1, kum) + subsetSUM(v, n - 1, kum - v[n - 1]);
 }
 
 int main()
 {
     int n = 7;
-    vector<int> v = {5, 6, 7, 8, 9, 3, 7};
+    int v[] = {5, 6, 2, 8, 9, 3, 12};
     int kum = 14;
     cout << subsetSUM(v, n, kum);
     return 0;
