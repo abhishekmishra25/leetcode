fb6a520eb39405efcefa50edb52a1d0c127aaef2
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-11 13:47:51 +0530

    Update 11.Josephus.cpp

--- 5.Recursion/11.Josephus.cpp
+++ 5.Recursion/11.Josephus.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 // In this problem we are given n number of
 // person , each time we have to kill the kth person
+// in this  problem it is hard to understand the
+// the problem with help of recursion tree
+// so we need to conceptulasie with the help of
+// diagram
 
 int josephus(int n, int k)
 {
