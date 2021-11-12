2d7e936bb433727578852ac6b9a9355c2e6a2d96
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-12 11:20:56 +0530

    Create 5.LeftRotateArray.cpp

---
+++ 2.array/5.LeftRotateArray.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // left rotate array by d
+    vector<int> arr = {3, 4, 5, 6, 3, 1, 2};
+    int d = 3;
+    while (d--)
+    {
+        arr.push_back(arr[0]);
+        erase(arr[0]);
+    }
+    for (auto x : arr)
+    {
+        cout << x << " ";
+    }
+    return 0;
+}
