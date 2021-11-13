1e3f1299916d1c028f56556eea26060a3e07fb54
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-13 07:18:57 +0530

    Update 5.LeftRotateArray.cpp

--- 2.array/5.LeftRotateArray.cpp
+++ 2.array/5.LeftRotateArray.cpp
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+class soluction
+{
+    int leftrotate(vector<int> arr) public : int rotateARR(vector<int> &arr, int d)
+    {
+        for (int i = 0; i < d; i++)
+        {
+            int temp = arr[0];
+        }
+    }
+}
 
-int main()
+int
+main()
 {
     // left rotate array by d
     vector<int> arr = {3, 4, 5, 6, 3, 1, 2};
     int d = 3;
-    while (d--)
-    {
-        arr.push_back(arr[0]);
-        arr.e
-            erase(arr[0]);
-    }
-    for (auto x : arr)
-    {
-        cout << x << " ";
-    }
-    return 0;
 }
