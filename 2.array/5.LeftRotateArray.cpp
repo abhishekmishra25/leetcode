23a8567e4c202bcfe62f8e87a938381d9e4e9990
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-14 13:57:18 +0530

    updated

--- 2.array/5.LeftRotateArray.cpp
+++ 2.array/5.LeftRotateArray.cpp
@@ -1,36 +1,45 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-leftrotate(int &arr, int n)
+class sol_Nive
 {
-    int temp = arr[0];
-    for (int i = 1; i < n; i++)
+public:
+    void print(int arr[], int n)
     {
-        arr[i - 1] = arr[i];
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
     }
-    arr[n - 1] = temp;
+    leftrotate(int arr[], int n)
+    {
+        int temp = arr[0];
+        for (int i = 1; i < n; i++)
+        {
+            arr[i - 1] = arr[i];
+        }
+        arr[n - 1] = temp;
+        // return arr;
+    }
+
+    void rotateARR(int arr[], int d, int n)
+    {
+        for (int i = 0; i < d; i++)
+        {
+            leftrotate(arr, n);
+        }
+        print(arr, n);
+    }
+
     // return arr;
-}
-
-void rotateARR(int &arr[], int d, int n)
-{
-    for (int i = 0; i < d; i++)
-    {
-        leftrotate(arr, n);
-    }
-}
-// return arr;
-
+};
 int main()
 {
     // left rotate array by d
     int arr[] = {3, 4, 5, 6, 3, 1, 2};
     int d = 3;
-    rotateARR(arr, d, 7);
-    for (int i = 0; i < 7; i++)
-    {
-        cout << arr[i] << " ";
-    }
+    sol_Nive ob1;
+    ob1.rotateARR(arr, d, 7);
+
     return 0;
 }
