225915dd225bbfa8f330d42054ec693258319d3b
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-14 13:49:11 +0530

    Update 5.LeftRotateArray.cpp

--- 2.array/5.LeftRotateArray.cpp
+++ 2.array/5.LeftRotateArray.cpp
@@ -1,40 +1,35 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-class soluction
-{
-public:
-    int leftrotate(vector<int> arr)
-    {
-        int temp = arr[0];
-        for (int i = 1; i < arr.size(); i++)
-        {
-            arr[i - 1] = arr[i];
-        }
-        arr[n - 1] = temp;
-        return arr;
-    }
 
-public:
-    int rotateARR(vector<int> arr, int d)
+leftrotate(int &arr, int n)
+{
+    int temp = arr[0];
+    for (int i = 1; i < n; i++)
     {
-        for (int i = 0; i < d; i++)
-        {
-            leftrotate(arr);
-        }
+        arr[i - 1] = arr[i];
     }
-    return arr;
+    arr[n - 1] = temp;
+    // return arr;
 }
 
-int
-main()
+void rotateARR(int &arr, int d, int n)
+{
+    for (int i = 0; i < d; i++)
+    {
+        leftrotate(arr, n);
+    }
+}
+// return arr;
+
+int main()
 {
     // left rotate array by d
-    vector<int> arr = {3, 4, 5, 6, 3, 1, 2};
+    int arr[] = {3, 4, 5, 6, 3, 1, 2};
     int d = 3;
-    soluction ob;
-    ob.rotateARR(arr, d);
-    for (int i = 0; i < arr.size(); i++)
+    soluction1 ob;
+    ob.rotateARR(arr, d, 7);
+    for (int i = 0; i < 7; i++)
     {
         cout << arr[i] << " ";
     }
