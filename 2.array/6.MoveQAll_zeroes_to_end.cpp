5194d9f7ccd48540122f98155ea496fe40c7e8f0
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-16 05:41:45 +0530

    Update 6.MoveQAll_zeroes_to_end.cpp

--- 2.array/6.MoveQAll_zeroes_to_end.cpp
+++ 2.array/6.MoveQAll_zeroes_to_end.cpp
@@ -1,4 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int mainn() {}
+class soluction_Nive
+{
+public:
+    void move_zero(int arr[], int n)
+    {
+        //
+    }
+};
+int mainn()
+{
+    int arr[] = {1, 2, 0, 7, 0, 3, 6, 4, 0, 0, 0};
+    int n = 11;
+}
