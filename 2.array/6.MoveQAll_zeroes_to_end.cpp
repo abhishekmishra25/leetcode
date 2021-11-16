fa802f7899ef3bb88a569b424c2c9c22526a7188
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-16 05:50:48 +0530

    Update 6.MoveQAll_zeroes_to_end.cpp

--- 2.array/6.MoveQAll_zeroes_to_end.cpp
+++ 2.array/6.MoveQAll_zeroes_to_end.cpp
@@ -5,11 +5,28 @@ class soluction_Nive
 public:
     void move_zero(int arr[], int n)
     {
-        //
+        // working in time complwxtiy of O(N^2);
+        for (int i = 0; i < n; i++)
+        {
+            // int temp = arr[i];
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[i] == 0 and arr[j] != 0)
+                {
+                    swap(arr[i], arr[j]);
+                }
+            }
+        }
     }
 };
 int mainn()
 {
     int arr[] = {1, 2, 0, 7, 0, 3, 6, 4, 0, 0, 0};
     int n = 11;
+    soluction_Nive ob1;
+    ob1.move_zero(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
 }
