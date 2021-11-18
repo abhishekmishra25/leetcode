e02f9c5543a254919e18fecc835cde5abdbd3aac
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-18 07:16:44 +0530

    Update 10.RainWaterTrappoing.cpp

--- 2.array/10.RainWaterTrappoing.cpp
+++ 2.array/10.RainWaterTrappoing.cpp
@@ -29,7 +29,7 @@ void getwater(int arr[], int n)
     lmax[0] = arr[0];
     for (int i = 1; i < n; i++)
     {
-        lmax[i] = max(arr[i], lmax[i]);
+        lmax[i] = max(arr[i], lmax[i - 1]);
     }
     rmax[n - 1] = arr[n - 1];
     for (int i = n - 2; i >= 0; i--)
