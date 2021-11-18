1ca2526489a3fee4c7281921b542ef5cff081dc6
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-18 07:47:31 +0530

    updated

--- 2.array/12.maxSUMsubarray.cpp
+++ 2.array/12.maxSUMsubarray.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+void getsum(int arr[], int n)
+{
+    int res = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        int curr = 0;
+        for (int j = i; i < n; j++)
+        {
+            curr += arr[j];
+            res = max(res, curr);
+        }
+    }
+    cout << res;
+}
 // applying kaddanes algorithm
-
 int maxSum(int arr[], int n)
 {
     int curr_sum = arr[0];
@@ -28,5 +41,6 @@ int main()
     int arr[] = {1, -2, 3, -1, 2};
     int n = 5;
     cout << maxSum(arr, n);
+    cout << getsum(arr, n);
     return 0;
 }
