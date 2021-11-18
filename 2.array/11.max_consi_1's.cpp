5f657ac937a8ebaf0dd7c90ea18cda0cea999df1
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-18 07:23:43 +0530

    Create 11.max_consi_1's.cpp

---
+++ 2.array/11.max_consi_1's.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int maxcon1(int arr[], int n)
+{
+    int curr_count = 0, max_count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 1)
+        {
+            curr_count += 1;
+        }
+        else
+        {
+            curr_count = 0;
+        }
+        if (curr_count > max_count)
+        {
+            max_count = curr_count;
+        }
+    }
+    return max_count;
+}
+
+int main()
+{
+    int arr[] = {0, 1, 1, 0, 1, 0, 1, 1, 1};
+    int n = 9;
+    maxcon1(arr, n);
+    return 0;
+}
