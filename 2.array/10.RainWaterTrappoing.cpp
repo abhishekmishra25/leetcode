e4f06e7e0a4c51a22d7512542483865bd27b0595
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-18 07:00:50 +0530

    updated

--- 2.array/10.RainWaterTrappoing.cpp
+++ 2.array/10.RainWaterTrappoing.cpp
@@ -2,61 +2,52 @@
 
 using namespace std;
 
-class Nive_sol
+void rainwater(int arr[], int n)
 {
-public:
-    void rainwater(int arr[], int n)
+    int res = 0;
+    for (int i = 1; i < n - 1; i++)
     {
-        int res = 0;
-        for (int i = 1; i < n - 1; i++)
+        int lmax = arr[i];
+        for (int j = 0; i < i; j++)
         {
-            int lmax = arr[i];
-            for (int j = 0; i < i; j++)
-            {
-                lmax = max(arr[j], lmax);
-            }
-            int rmax = arr[i];
-            for (int j = i + 1; j < n; j++)
-            {
-                rmax = max(arr[j], rmax);
-            }
-            res = res + (min(lmax, rmax) - arr[i]);
+            lmax = max(lmax, arr[j]);
         }
-        cout << res << endl;
+        int rmax = arr[i];
+        for (int j = i + 1; j < n; j++)
+        {
+            rmax = max(rmax, arr[j]);
+        }
+        res = res + (min(lmax, rmax) - arr[i]);
     }
-};
-class better_sol
+    cout << res << endl;
+}
+
+void getwater(int arr[], int n)
 {
-public:
-    void getwater(int arr[], int n)
+    int res = 0;
+    int lmax[n], rmax[n];
+    lmax[0] = arr[0];
+    for (int i = 1; i < n; i++)
     {
-        int res = 0;
-        int lmax[n], rmax[n];
-        lmax[0] = arr[0];
-        for (int i = 1; i < n; i++)
-        {
-            lmax[i] = max(arr[i], lmax[i]);
-        }
-        rmax[n - 1] = arr[n - 1];
-        for (int i = n - 2; i >= 0; i--)
-        {
-            rmax[i] = max(arr[i], rmax[i + 1]);
-        }
-        for (int i = 1; i < n - 1; i++)
-        {
-            res = res + min(lmax[i], rmax[i]) - arr[i];
-        }
-        cout << res;
+        lmax[i] = max(arr[i], lmax[i]);
     }
-};
+    rmax[n - 1] = arr[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        rmax[i] = max(arr[i], rmax[i + 1]);
+    }
+    for (int i = 1; i < n - 1; i++)
+    {
+        res = res + min(lmax[i], rmax[i]) - arr[i];
+    }
+    cout << res << endl;
+}
 
 int main()
 {
-    int arr[] = {3, 0, 1, 2, 5};
+    int arr[] = {5, 0, 6, 2, 3};
     int n = 5;
-    Nive_sol ov1;
-    ov1.rainwater(arr, n);
-    better_sol ob2;
-    ob2.getwater(arr, n);
+    rainwater(arr, n);
+    getwater(arr, n);
     return 0;
 }
