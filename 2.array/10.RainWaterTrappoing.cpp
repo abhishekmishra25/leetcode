d5dc75399b9e4d661f622d506d96be394377ad4f
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-18 06:48:24 +0530

    added

--- 2.array/10.RainWaterTrappoing.cpp
+++ 2.array/10.RainWaterTrappoing.cpp
@@ -25,6 +25,30 @@ public:
         cout << res << endl;
     }
 };
+class better_sol
+{
+public:
+    void getwater(int arr[], int n)
+    {
+        int res = 0;
+        int lmax[n], rmax[n];
+        lmax[0] = arr[0];
+        for (int i = 1; i < n; i++)
+        {
+            lmax[i] = max(arr[i], lmax[i]);
+        }
+        rmax[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+        {
+            rmax[i] = max(arr[i], rmax[i + 1]);
+        }
+        for (int i = 1; i < n - 1; i++)
+        {
+            res = res + min(lmax[i], rmax[i]) - arr[i];
+        }
+        cout << res;
+    }
+};
 
 int main()
 {
@@ -32,5 +56,7 @@ int main()
     int n = 5;
     Nive_sol ov1;
     ov1.rainwater(arr, n);
+    better_sol ob2;
+    ob2.getwater(arr, n);
     return 0;
 }
