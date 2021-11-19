6778fd9395ea3368dbe256f6a124151e8fe94904
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-19 16:38:48 +0530

    Update 13.LengthOfMaxEvenOddSubarray.cpp

--- 2.array/13.LengthOfMaxEvenOddSubarray.cpp
+++ 2.array/13.LengthOfMaxEvenOddSubarray.cpp
@@ -1,7 +1,20 @@
 #include <bits/stdc++h>
 using namespace std;
 
+// create a function which tell max length of
+// even odd occurring subaaray
+
+int MaxlenSubArray(int arr[], int n)
+{
+    int count = 0, rsult = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0 and arr[i] % 2 == 0)
+    }
+}
+
 int main()
 {
     int arr[] = {10, 12, 14, 7, 9};
+    int n = 5;
 }
