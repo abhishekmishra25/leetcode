5bdfba3667a093deb5b812c1d99f7f12469578af
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-20 14:06:55 +0530

    updated

--- 2.array/13.LengthOfMaxEvenOddSubarray.cpp
+++ 2.array/13.LengthOfMaxEvenOddSubarray.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // create a function which tell max length of
@@ -6,15 +6,26 @@ using namespace std;
 
 int MaxlenSubArray(int arr[], int n)
 {
-    int count = 0, rsult = 0;
+    int rsult = 1;
     for (int i = 0; i < n; i++)
     {
-        if (i % 2 == 0 and arr[i] % 2 == 0)
+        int curr = 1;
+        for (int j = i + 1; j < n; j++)
+        {
+            if ((arr[i] % 2 == 0 and arr[i - 1] % 2 != 0) || (arr[i] % 2 != 0 and arr[i - 1] % 2 == 0))
+                curr++;
+            else
+                break;
+        }
+        rsult = max(rsult, curr);
     }
+    return rsult;
 }
 
 int main()
 {
-    int arr[] = {10, 12, 14, 7, 9};
+    int arr[] = {10, 12, 13, 4, 9};
     int n = 5;
+    cout << MaxlenSubArray(arr, n);
+    return 0;
 }
