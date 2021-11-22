0c056950dc10ed0f6e55fab6957bef0c3338756a
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-22 15:22:59 +0530

    Update 15.findMajority.cpp

--- 2.array/15.findMajority.cpp
+++ 2.array/15.findMajority.cpp
@@ -19,6 +19,25 @@ int findMajority(int arr[], int n)
     }
     return -1;
 }
+
+// So, I want to solve this problem in O(N)
+//
+struct myCmp
+{
+public:
+    void good(map<int, int> p1, map<int, int> p2)
+    {
+        if (p1.se)
+    }
+} int findMajority(int arr[], int n)
+{
+    mao<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        ma[arr[i]]++;
+    }
+    sort(mp.begin(), mp.end(), myCmp);
+}
 int main()
 {
     int arr[] = {8, 7, 6, 8, 6, 6, 6, 6, 6};
