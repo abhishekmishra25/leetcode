cb118e5edfcc7054619992ff8846c2da3ff9e5ec
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-22 07:20:49 +0530

    Create 15,findMajority.cpp

---
+++ 2.array/15,findMajority.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int findMajority(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int count = 1;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+                count++;
+        }
+        if (count > n / 2)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int arr[] = {8, 7, 6, 8, 6, 6, 6, 6, 6};
+    int n = 7;
+    cout << findMajority(arr, n) << endl;
+}
