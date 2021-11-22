52c041dc7eee02cac7eb8583e96a12bed7cf1f2f
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-22 07:10:47 +0530

    Update 14maxSumCircularArray.cpp

--- 2.array/14maxSumCircularArray.cpp
+++ 2.array/14maxSumCircularArray.cpp
@@ -24,9 +24,23 @@ int maxSum(int arr[], int n)
 //  so what i'm thinking is if i mutlipky all the
 //  elements by -1 then find max sum
 //  then add to the total sum;
+int normalMax(int arr[], int n)
+{
+    int curr_sum = arr[0];
+    int max_sum = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        curr_sum = max(curr_sum + arr[i], arr[i]);
+        max_sum = max(curr_sum, max_sum);
+    }
+    return max_sum;
+}
 
 int revKadane(int arr[], int n)
 {
+    int k = normalMax(arr, n);
+    if (k < 0)
+        return k;
     int sum = 0;
     for (int i = 0; i < n; i++)
     {
@@ -58,3 +72,5 @@ int main()
     cout << revKadane(arr2, n2);
     return 0;
 }
+// time complexity on O(3*N) = O(N);
+// goodbye
