da66742daf4c0b00a75336debeae587fc8373afb
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-22 06:23:19 +0530

    Update 14maxSumCircularArray.cpp

--- 2.array/14maxSumCircularArray.cpp
+++ 2.array/14maxSumCircularArray.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int maxSum(int arr[], int n)
 {
+    int res = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        int curr_sum = arr[i];
+        int max_sum = arr[i];
+        for (int j = 1; j < n; j++)
+        {
+            int idx = (i + j) % n;
+            curr_sum += arr[idx];
+            max_sum = max(max_sum, curr_sum);
+        }
+        res = max(max_sum, res);
+    }
+    return res;
 }
 
 int main()
