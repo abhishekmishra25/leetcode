8e581c01079ff250253d32eaa6846fdb8e0531ed
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-22 06:35:46 +0530

    Update 14maxSumCircularArray.cpp

--- 2.array/14maxSumCircularArray.cpp
+++ 2.array/14maxSumCircularArray.cpp
@@ -20,10 +20,35 @@ int maxSum(int arr[], int n)
     return res;
 }
 
+// solving same problem using kaddane algortihm
+//  so what i'm thinking is if i mutlipky all the
+//  elements by -1 then find max sum
+//  then add to the total sum;
+
+int revKadane(int arr[], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+        arr[i] = arr[i] * (-1);
+    }
+    int curr_sum = arr[0];
+    int max_sum = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        curr_sum = max(curr_sum + arr[i], arr[i]);
+        max_sum = max(curr_sum, max_sum);
+    }
+    return sum + max_sum;
+}
+
 int main()
 {
     int arr[] = {5, -2, 3, 4};
     int n = 4;
     cout << maxSum(arr, n);
+    cout << endl;
+    cout << revKadane(arr, n);
     return 0;
 }
