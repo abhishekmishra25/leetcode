de9804f38396721d5e4fe61f47415a8b66210777
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-23 19:25:35 +0530

    update

--- 2.array/16.MinGroupFlip.cpp
+++ 2.array/16.MinGroupFlip.cpp
@@ -2,8 +2,15 @@
 
 using namespace std;
 
+void MinGroupFlip(int arr[], int n)
+{
+    cout << "From 2 to 4" << endl;
+}
+
 int main()
 {
     int arr[] = {1, 1, 0, 0, 0, 1};
     int n = 6;
+    MinGroupFlip(arr, n);
+    return 0;
 }
