d3f99247f8550abfc3ea4c736d04cfaaaca6a329
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-24 06:56:26 +0530

    updated

--- 2.array/16.MinGroupFlip.cpp
+++ 2.array/16.MinGroupFlip.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+/*
 int count_group(int arr[], int n, int a)
 {
-    int counta;
+    int counta=0;
     for (int i = 1; i < n; i++)
     {
         if ((arr[i] == a) and (arr[i - 1]) != a)
@@ -21,13 +21,34 @@ void MinGroupFlip(int arr[], int n)
     int res = max(c0, c1);
     for (int i = 0; i < n; i++)
     {
+        if(arr[i]==res)
+    }
+}*/
+
+void solve(int arr[], int n)
+{
+    int start = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] != arr[i - 1])
+        {
+            if ((arr[i] != start))
+                cout << "From " << i << " to ";
+            else
+                cout << (i - 1) << endl;
+        }
+        // if ((arr[i + 1] == start) and (arr[i] != start))
+        //     cout << i << endl;
+    }
+    if (arr[n - 1] != start)
+    {
+        cout << (n - 1) << endl;
     }
 }
-
 int main()
 {
-    int arr[] = {1, 1, 0, 0, 0, 1};
+    int arr[] = {1, 1, 0, 0, 1, 1, 0, 0, 1};
     int n = 6;
-    MinGroupFlip(arr, n);
+    solve(arr, n);
     return 0;
 }
---
+++ 2.array/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+rt) and (arr[i] != start))
+        //     cout << i << endl;
