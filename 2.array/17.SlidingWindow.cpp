8d5cf9e5ce8b41b55bf8d2f9c2edd47943956c7c
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-11-24 07:11:32 +0530

    Update 17.SlidingWindow.cpp

--- 2.array/17.SlidingWindow.cpp
+++ 2.array/17.SlidingWindow.cpp
@@ -1,10 +1,27 @@
 #include <bits/stdc++.h>
 
+void solve_sliding_window(int arr[] int n, int k)
+{
+    int sum = 0;
+    for (int i = 0; i < k; i++)
+    {
+        sum += arr[i];
+    }
+    for (int i = k; i < n; i++)
+    {
+        int curr = sum + arr[i] - arr[i - k];
+        sum = max(sum, curr);
+    }
+    cout << sum;
+    return;
+}
 using namespace std;
 
 int main()
 {
 
-    int arr[] = {};
-    int n = 0;
+    int arr[] = {1, 8, 30, -5, 20, 7};
+    int n = 6;
+    solve_sliding_window(arr, n, 3);
+    return 0;
 }
