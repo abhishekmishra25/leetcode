6bea6b54eff69216cac9e0c527927b58a1ed8311
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-12-05 20:02:53 +0530

    Update 3.countOccur.cpp

--- 6.Searching/3.countOccur.cpp
+++ 6.Searching/3.countOccur.cpp
@@ -5,6 +5,26 @@ using namespace std;
 class soluction
 {
 public:
+    int FirstOccur(vector<int> &v, int n, int target)
+    {
+        int low = 0, high = n - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (v[mid] > target)
+                high = mid - 1;
+            else if (v[mid] < target)
+                low = mid + 1;
+            else
+            {
+                if (mid == 0 || v[mid] != v[mid - 1])
+                    return mid;
+                else
+                    high = mid - 1;
+            }
+        }
+        return -1;
+    }
     int Lastindex(vector<int> &arr, int target)
     {
         int low = 0, high = arr.size() - 1;
@@ -27,8 +47,8 @@ public:
     }
     int CountOccr(vector<int> &arr, int target)
     {
-        int low = 0, high = arr.size() - 1;
-        wile(low)
+        int low = FirstOccur(arr, arr.size(), target), high = Lastindex(arr, target);
+        return low - high;
     }
 };
 
@@ -37,6 +57,8 @@ int main()
 {
     vector<int> arr = {10, 20, 20, 20, 20, 30, 30};
     int target = 20;
-
+    soluction ob1;
+    cout << ob1.CountOccr(arr, target) << endl;
+    // ;
     return 0;
 }
