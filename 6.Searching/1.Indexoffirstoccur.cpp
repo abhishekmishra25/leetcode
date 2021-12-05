a2b3e1f4fac0b3e5919121604d50e61a42edad39
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-12-05 19:08:34 +0530

    Create 1.Indexoffirstoccur.cpp

---
+++ 6.Searching/1.Indexoffirstoccur.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class soluction
+{
+public:
+    int FirstOccur(vector<int> &v, int n, int target)
+    {
+        int low = 0, high = n - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (v[mid] > target)
+                high = mid - 1;
+            else if (v[mid] < target)
+                low = mid + 1;
+            else
+            {
+                if (mid == 0 || v[mid] != v[mid - 1])
+                    return mid;
+                else
+                    high = mid - 1;
+            }
+        }
+        return -1;
+    }
+} int main()
+{
+    vector<int> v = {10, 10, 20, 10, 20, 30};
+    int n = 6;
+}
