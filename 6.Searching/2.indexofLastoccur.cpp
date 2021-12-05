411a3705c9b7ce0ba5d4be63d6c200d9e2de7599
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2021-12-05 19:18:08 +0530

    Create 2.indexofLastoccur.cpp

---
+++ 6.Searching/2.indexofLastoccur.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// creating a calss soluction
+class soluction
+{
+public:
+    int Lastindex(vector<int> &arr, int target)
+    {
+        int low = 0, high = arr.size() - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] > target)
+                high = mid - 1;
+            else if (arr[mid] < target)
+                low = mid + 1;
+            else
+            {
+                if (mid == arr.size() - 1 || arr[mid] != arr[mid + 1])
+                    return mid;
+                else
+                    low = mid + 1;
+            }
+        }
+    }
+};
+
+// main  function
+int main()
+{
+    vector<int> arr = {10, 10, 20, 20, 30, 40};
+    int target = 30;
+}
