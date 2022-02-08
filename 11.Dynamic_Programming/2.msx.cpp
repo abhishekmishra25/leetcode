f00e998f967ca31e3057553941f7626c2b3102a1
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2022-02-08 10:43:23 +0530

    updated

--- 11.Dynamic_Programming/2.msx.cpp
+++ 11.Dynamic_Programming/2.msx.cpp
@@ -2,33 +2,43 @@
 
 using namespace std;
 
-int main()
+class solution
 {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++)
-        cin >> v[i];
-
-    map<int, int> mp;
-    set<int> s(v.begin(), v.end());
-    for (int i = 0; i < n; i++)
+private:
+    double multiply(double mid, int n)
     {
-        mp[v[i]]++;
-    }
-    int count = 0;
-    while (mp.size != 0)
-    {
-        for (auto x : s)
+        double ans = 1;
+        for (int i = 0; i < n; i++)
         {
-            count++;
-            mp.erase(x);
-            if (s.find(x + 1) == true)
-                mp.erase(x + 1);
-            if (s.find(x - 1) == true)
-                mp.erase(x - 1);
+            ans *= mid;
         }
+        return ans;
     }
-    cout << count;
+
+public:
+    void NthrootOfM(int n, int m)
+    {
+        double high = m, low = 0;
+        double esp = 1e-6;
+        while (high - low > esp)
+        {
+            double mid = low + (high - low) / 2.0;
+            if (multiply(mid, n) < m)
+                low = mid;
+            else
+                high = mid;
+        }
+        cout << "Output by binary searrch" << endl;
+        cout << low << "  " << high << endl;
+        cout << " Output by fromula" << endl;
+        cout << pow(m, (double)(1.0 / (double)n));
+    }
+};
+signed main()
+{
+    int n, m;
+    cin >> n >> m;
+    solution oj;
+    oj.NthrootOfM(n, m);
     return 0;
 }
