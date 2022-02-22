e03fde1d09c7df0e030c9da7b23c5fcf6b5ad747
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2022-02-22 06:59:48 +0530

    updated

--- 11.Dynamic_Programming/3.lcs.cpp
+++ 11.Dynamic_Programming/3.lcs.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+// normal recursion
 class solution
 {
 public:
@@ -16,6 +16,34 @@ public:
             return max(lcs(s1, s2, m - 1, n), lcs(s1, s2, m, n - 1));
     }
 };
+// tabulation
+class dyanmic
+{
+public:
+    int lcs(string s1, string s2, int m, int n)
+    {
+        int dp[m + 1][n + 1];
+        for (int i = 0; i <= m; i++)
+        {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= n; i++)
+        {
+            dp[0][i] = 0;
+        }
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (s1[i - 1] == s2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m][n];
+    }
+};
 
 int main()
 {
@@ -23,5 +51,9 @@ int main()
     int m = s1.length(), n = s2.length();
     solution ob1;
     cout << ob1.lcs(s1, s2, m, n);
+    cout << endl;
+    dyanmic ob2;
+    cout << ob2.lcs(s1, s2, m, n);
+
     return 0;
 }
