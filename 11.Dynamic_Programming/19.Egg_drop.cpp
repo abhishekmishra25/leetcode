004b505a2456e8cf6be167c973dac42b726b7244
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2022-03-25 18:16:38 +0530

    updated

--- 11.Dynamic_Programming/19.Egg_drop.cpp
+++ 11.Dynamic_Programming/19.Egg_drop.cpp
@@ -7,18 +7,35 @@ using namespace std;
 class solution
 {
 public:
+    // resursive solution
+    // In this question we have to find the minimum number of trails for
+    // finding the three should floor for drooping egg
     int egg_drop_rec(int floor, int egg)
     {
+        // we have to write the base case first
+        // if thre is only one egg left then it does not make any sence
+        // to check in random binary order
         if (egg == 1)
             return floor;
+        // if there is only one or zero floor are left then
+        // It will take only 1 or 0 moves
         if (floor == 1)
             return 1;
         if (floor == 0)
             return 0;
-        int res = INT_MAX;
-        for (int x = 1; x < floor; x++)
-            res = min(max(egg_drop_rec(x - 1, egg - 1), egg_drop_rec(floor - x, egg)), res);
-        return res;
+        // Now initializing the variables
+        // To get min threeshould floor
+        // we have to check for both the condition if egg break or not break
+        int mi = INT_MAX, res;
+        for (int x = 1; x <= floor; x++)
+        {
+            //                      if egg break        or         not break
+            res = max(egg_drop_rec(x - 1, egg - 1), egg_drop_rec(floor - x, egg));
+            // checking if if the current floor is threeshould floor or not
+            if (res < mi)
+                mi = res;
+        }
+        return mi + 1;
     }
 };
 
