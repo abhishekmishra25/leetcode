f02271bfb64b9800f716793c435a8d56752a3f1f
Abhishek Mishra <55997209+abhishekmishra25@users.noreply.github.com>
2022-04-19 21:47:22 +0530

    Create 2.shortestPathInDAG.cpp

---
+++ 12.Graph/2.shortestPathInDAG.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+void generate_graph(vector<int> graph[], int u, int v)
+{
+    graph[u].push_back(v);
+}
+
+int main()
+{
+    vector<int> graph[4];
+    for (int i = 0; i < 4; i++)
+    {
+    }
+}
